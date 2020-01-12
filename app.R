1c3f9bcbdf2b381e520e47169c72b6cd549b9593
Nathan Otto Siemers <nathan.siemers@bms.com>
2020-01-12 13:45:10 -0800

    fixed coloring stability - tried to add all/none buttons but failed. tried to adjust checkbox checks by what is filtered in other fields - failed. still need to catch and kill infinite cycles.

--- app.R
+++ app.R
@@ -6,6 +6,8 @@ if(! require('shinyCustom')) {
 }
 library(shinyCustom)
 library(shinycssloaders)
+unique_categorical = 13  ## less than this and we make categories instead of sliders
+max_hist_cat = 20  ## above this, we don't show categorical histograms
 ################################################################
 ## testing dataset 
 data = as.data.frame(storms)
@@ -37,27 +39,30 @@ ui = fluidPage(
         sidebarPanel(
             actionButton('go', 'Plot'),
             tags$br(), tags$br(),
+            ################################################################
+            ## needed for thanos
             selectizeInput('selectthings', 'select things',
                            choices = NULL, multiple = TRUE),
             uiOutput('thanos'),
+            ################################################################
             tags$br(), tags$br(),
             verbatimTextOutput('print2'),
             verbatimTextOutput('print1')
             ),
+        ### this stuff below is just to draw an example graph, not part of thanos
         mainPanel(
             div(withSpinner(plotOutput( 'plotter', width = '100%', height = 700 ))),
             splitLayout(
                 selectizeInput('plotthings', 'graph things',
-                               choices = NULL, multiple = TRUE), 
+                               choices = NULL, multiple = TRUE),
                 selectizeInput('colorthings', 'color by',
                                choices = NULL),
                 selectizeInput('facetthings', 'graph for each', 
                                choices = NULL),
                 tags$head(tags$style(HTML("
-    .shiny-split-layout > div {
-      overflow: visible;
-    }
-  ")))
+                 .shiny-split-layout > div {
+                 overflow: visible;
+                 }" )))
                 ),
             tags$br(), tags$br(),
             tags$br(), tags$br(),
@@ -87,7 +92,7 @@ server = function( input, output, session ) {
             if(
                 ##(! is.numeric( mydata[, i] ) )
                 (! is.numeric( mydata[, i] ) )  |
-                length(unique( mydata[, i] ) ) <=  9 
+                length(unique( mydata[, i] ) ) <=  unique_categorical 
                 ) {
                 if(!is.null( input[[i]] ) ) {
                     mydata = mydata[ mydata[, i] %in% input[[i]], ]
@@ -102,6 +107,13 @@ server = function( input, output, session ) {
         }
         mydata
     }
+    ## observe({
+    ##     for( i in input$selectthings ) {
+    ##         input[[ paste0('selectall_', i) ]]
+    ##         choices = sort(unique(data[,i]))
+    ##         updateCheckboxGroupInput(session, i, i, choices= choices, selected = isolate(choices), inline = TRUE)
+    ##     }
+    ## })
     observe({
         Map(function(name) {
             print("Map")
@@ -109,9 +121,9 @@ server = function( input, output, session ) {
             if( is.null( input[[name]] ) )  return(NULL)
             selectthings = input$selectthings
             print(paste('selectthings:', selectthings))
-            if( 
+            if(
                 is.numeric( data[, name] )  &
-                length(unique( data[, name] ) ) > 9 
+                length(unique( data[, name] ) ) > unique_categorical 
                 ) {
                 print('we are here')
                 minvar = min(data[,name], na.rm = TRUE)
@@ -122,17 +134,36 @@ server = function( input, output, session ) {
                 if ( length(input[[name]]) == 2 ) {
                     one = input[[name]][[1]]; two = input[[name]][[2]]
                     filtering = data[,name] >= one & data[,name] <=  two
+                    filtering = factor(ifelse( filtering, 'sel', 'unsel'), levels = c('sel', 'unsel') )
                     output[[paste0('hist_', name)]] <-  renderPlot({
                         p = ggplot( data ) +
                             geom_histogram( bins = 60, aes_string(
                                                 x = name,
                                                 fill = filtering
-                                ) ) + theme_hist + scale_fill_gdocs() +
+                                ) ) + theme_hist + scale_fill_gdocs(drop = FALSE) +
                                     xlim(minvar, maxvar)
                         p
                     }) 
                 }
-            } },
+            } else {
+                print("entering categorical histogram")
+                mostthings = input$selectthings
+                mostthings = mostthings[ mostthings != name ]
+                data = getData(data, mostthings)
+                if( length(which( data[, name] %in% input[[ name]])) == length(data[,name])) {}
+                filtering = data[,name] %in% input[[ name ]]
+                filtering = factor(ifelse( filtering, 'sel', 'unsel'), levels = c('sel', 'unsel') )
+                print(head(filtering))
+                output[[paste0('hist_', name)]] <-  renderPlot({
+                    p = ggplot( data ) +
+                        geom_histogram( stat="count", aes_string(
+                                            x = name,
+                                            fill = filtering
+                            ) ) + theme_hist + scale_fill_gdocs(drop = FALSE) 
+                    p
+                })
+            }
+        },
             input$selectthings )
     })
     p = eventReactive( input$go, { 
@@ -165,8 +196,11 @@ server = function( input, output, session ) {
     output$thanos = renderUI({
         print('enter outpu$thanos')
         out= tagList()
+        mydata = getData(data)
         for ( i in input$selectthings )   {
-            if( is.numeric( data[, i] ) ) {
+            things_in_i = unique( mydata[, i] )
+            if( is.numeric( data[, i] ) &
+               length(unique(data[, i])) >= unique_categorical  ) {
                 minvar = min(data[,i], na.rm = TRUE)
                 maxvar  = max(data[,i], na.rm = TRUE)
                 step = signif( ( maxvar - minvar ) / 100, digits = 1 )
@@ -183,28 +217,43 @@ server = function( input, output, session ) {
                 out = tagAppendChild(out,
                     div(
                         useShinyCustom(
-                            slider_delay = "1000",
-                            numeric_delay = "1000",
-                            text_delay = "1000"
+                            slider_delay = "2000",
+                            numeric_delay = "2000",
+                            text_delay = "2000"
                             ), 
                         customSliderInput(i, i, min = minvar, max = maxvar,
-                                          ##round = TRUE,
                                           value = c(thisslider),
                                           width = 300 )
                         ))
                 out = tagAppendChild(out,
                     plotOutput( paste0('hist_', i), width = 300, height = 100) )
             } else {
-                varfun = debounce(reactive( input[[i]] ), millis = 3000)
+                varfun = debounce(reactive( input[[i]] ), millis = 1500)
+                if(is.null(varfun())) print( 'varfun is null')
+                print(head(paste( 'varfun categorical is:', varfun() )))
+                if( is.null(varfun() )) {
+                    myvar = sort(unique( mydata[ , i] ))
+                } else {
+                    myvar = varfun()
+                }
                 out = tagAppendChild ( out,
                     div(
                         checkboxGroupInput( i, i,
-                                           choices = sort(unique(data[,i])),
-                                           selected = varfun(),
+                                           choices = sort(unique( data[, i] )),
+                                           ##selected = myvar[ myvar %in% things_in_i ],
+                                           selected = myvar,
                                            inline = TRUE
                                            )
-                        )
-                                      )
+                        ##actionButton(
+                        ##    paste0('selectall_', i), 'All' )
+                        ##)
+                                      ) )
+                if ( length(unique(data[ , i] )) <= max_hist_cat ) {
+                    print( "less than max_hist_cat")
+                    print(max_hist_cat)
+                    out = tagAppendChild(out,
+                        plotOutput( paste0('hist_', i), width = 300, height = 100) )
+                }
             }
         }
         out
@@ -223,4 +272,5 @@ server = function( input, output, session ) {
         print(head(str(data), 40))
     })
 }
+
 shinyApp(ui, server)
