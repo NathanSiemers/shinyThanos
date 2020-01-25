477dad5da7c6302ffe0aed077f0b13edd3f35afb
Nathan Otto Siemers <nathan.siemers@bms.com>
2020-01-25 09:57:07 -0800

    cleaned up debugging prints, etc.

--- thanos.R
+++ thanos.R
@@ -2,7 +2,7 @@
 ## functions needed for thanos
 ################################################################
 ## this code plus a map creates all the dynamic filters
-library(viridis)
+library(viridis)  ## histogram plotter needs these colors
 
 theme_thanos = theme(axis.text.x = element_text(size = 12),  ####, angle = 30, hjust = 10)
     axis.text.y = element_text(size = 9),
@@ -21,7 +21,6 @@ ui_filters = function(x, selectedvals, var, picket, width = '100%', height = '20
     } else if (picket == 2) {
         if (is.numeric(x)) {
             rng <- range(x, na.rm = TRUE)
-            ##print(paste('range is:', rng))
             if( length(selectedvals) == 2 ) { ## we have values
                 sliderInput(var, var, min = rng[1], max = rng[2], value = selectedvals, width = width)
             } else {
@@ -29,9 +28,6 @@ ui_filters = function(x, selectedvals, var, picket, width = '100%', height = '20
             }
         } else {
             levs <- sort(unique((x)))
-            ##print(paste( "within checkbox creation: selectedvals is:", selectedvals ))
-            action = 1
-            ##if ( is.null(selectedvals) & FALSE ) {
             if ( length(selectedvals) < 1 ) {
                 checkboxGroupInput(var, var, choices = levs, selected = levs, inline = TRUE, width = width) 
             } else {
@@ -45,7 +41,6 @@ ui_filters = function(x, selectedvals, var, picket, width = '100%', height = '20
         }
     } else if (picket == 4 ) {
         plot_obj = paste0('plot_', var)
-        ##div(style = "height:100px;", plotOutput(plot_obj, height = 100,, width = width) )
         plotOutput(plot_obj, height = height, width = width)
     }  else if (picket == 5) {
     }
@@ -53,13 +48,7 @@ ui_filters = function(x, selectedvals, var, picket, width = '100%', height = '20
 
 
 filter_var <- function(data_list, var, val) {
-    ## print('in filter_var')
-    ## print(paste('variable is', var))
-    ## print(paste('value is', str(val)))
     l = lapply(data_list, function (dat)  {
-        ## print('head of dat')
-        ## print(head(dat))
-        ## print(colnames(dat))
         if ( var %in% colnames(dat)) {
             x = dat[ , var]
             if (is.numeric(x)) {
@@ -68,11 +57,8 @@ filter_var <- function(data_list, var, val) {
                 out = is.na(x) | x %in% val
             }
         } else {
-            ## print(paste('did not find',  var, 'in this df'))
             out = rep(TRUE, nrow(dat) )
         }
-        ## print('filter_var lapply')
-        ## print(table(out))
         out
     })
     Reduce("&", l)
@@ -91,31 +77,12 @@ get_cols_in_data_list = function(dl) {
 
 plot_it = function(data_list, selected, x){
     var = get_variable_in_data_list( data_list, x )
-    ## print("in plot_it")
-    ## print(paste("name is:", x))
-    ## print(paste('var length:', length(var)))
-    ## print(str(var))
-    ## print('selected:')
-    ## print(str(selected))
     filter_most = selected[ names(selected) != x  ]
-    ## print("filter_most")
-    ## print(str(filter_most))
     reduced = Reduce( "&", filter_most )
-    ## print('structure of reduced')
-    ## print(str(reduced))
     filtered_most = var[ reduced ]
     filter_last =  selected[[ x ]]
-    ## print("filter_last")
-    ## print(str(filter_last))
     filter_last = filter_last[ reduced ] 
-    ## print(length(filter_last))
-    ## print(str(filter_last))
     plot_name = paste0('plot_', x)
-    ## print('ready to plot')
-    ## print('x')
-    ## print(head(filtered_most))
-    ## print('fill')
-    ## print(head(filter_last))
     fcolor = factor(ifelse( filter_last, 'sel', 'unsel'), levels = c('sel', 'unsel') )
     plot_title = paste( x, ':', length(which(filter_last)),  '/', length(filtered_most) )
     if (is.numeric(var)) {
@@ -131,32 +98,21 @@ plot_it = function(data_list, selected, x){
     p + theme_thanos
 }
 
-## try to get thanos into function calls/libs
 thanos_selectize = function( input, output, session, data_list, default_selected = c() ) {
-    ## print('inside thanos_select')
     updateSelectizeInput(
         session, 'selectthings',  choices = get_cols_in_data_list(data_list),
         selected = default_selected, server = TRUE )
 }
+
 thanos_selected = function( input, output, session, data_list) {
-    ##print('in thanos selected')
     reactive({
-        ##req( input$selectthings )
-        ##map(input$selectthings, ~ print( input[[.x]] ) )
         each_var <- map(input$selectthings, ~ filter_var(data_list = data_list, .x, input[[.x]]) )
-        ##print('thanos_selected each_var')
-        ##print(head(each_var))
-        ##
-        ##reduce(each_var, `&`)
         names(each_var) = input$selectthings
         each_var
     })
 }
 
 thanos_histos = function( input, output, session, data_list, selected) {
-##    observe({
-    ##        map( input$selectthings, ~ input[[.x]] )
-    ## print("inside thanos_histos")
     cname = paste0('plot_', input$selectthings)
     Map(function(name) {
         cname = paste0('plot_', name)
@@ -165,15 +121,10 @@ thanos_histos = function( input, output, session, data_list, selected) {
         }) ##, res = 300 )
     },
         input$selectthings )
-    ## print('exit thanos_histos')
-    ##    }, priority = 5 )
-    ##    output
 }
 
 thanos_allnone = function(input, output, session, data_list, x) {
     act = input[[ paste0('action_', x) ]]
-    ## print('love/hate')
-    ## print(x)
     allchoices = sort(unique(get_variable_in_data_list( data_list, x ) ) )
     if ( as.numeric(act) %% 2 == 0 ) {
         return (updateCheckboxGroupInput( session, x, x, choices = allchoices, selected = allchoices, inline = TRUE ) )
@@ -182,19 +133,13 @@ thanos_allnone = function(input, output, session, data_list, x) {
     }
 }
 
-## this isn't used (yet!)
+## this isn't used yet, as I can't get it to work as a function called by shiny ?
+## something like this is currently written in the server part of app.R
 thanos_renderfilters = function(input, output, session, data_list, selectthings, selected, dups = 5) {
-    ## print('inside thanos_renderfilters')
     output$renderfilters <<- renderUI({
-        ## print('begin rep renderfilters')
         dupnames = unlist(lapply(input$selectthings,  function(x){ rep(x, dups) } ) )
-        ## print('end rep renderfilters')
         dupcounts = rep(1:dups, length( input$selectthings ) )
-        ##tagList(map2(dupnames, dupcounts, ~ ui_filters( data[[.x]], isolate(input[[.x]]), .x, data[selected, ], .y ) ))
         tagList(map2(dupnames, dupcounts, ~ ui_filters( get_variable_in_data_list( data_list, .x ), isolate(input[[.x]]), .x, .y ) ))
     })
 }
 
-################################################################
-## end of functions needed for thanos
-################################################################
